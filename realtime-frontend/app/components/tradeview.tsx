26c79194be60b901f9fdea32a8aec051cb1a595b
roshan1697 <roshanpoojary16@gmail.com>
2025-10-17 23:46:54 +0530

    pages created

---
+++ realtime-frontend/app/components/tradeview.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import { useEffect, useRef } from "react"
+import { ChartManager } from "../utils/chartmanager"
+import { KLine } from "../utils/types"
+import { getKlines } from "../utils/httpClient"
+
+const TradeView = ({ market }: { market: string }) => {
+    const chartRef = useRef<HTMLDivElement>(null)
+    const chartManagerRef = useRef<ChartManager>(null)
+
+    useEffect(() => {
+
+        const init = async () => {
+            let klinedata: KLine[] = []
+            try {
+                klinedata = await getKlines(market, '1hr', Math.floor((new Date().getTime() - 1000 * 60 * 60 * 24 * 7) / 1000), Math.floor(new Date().getTime() / 1000))
+            } catch (e) {
+
+            }
+            if(chartRef){
+                if(chartManagerRef.current){
+                    chartManagerRef.current.destroy()
+                }
+                
+                const chartManager = new ChartManager(
+                    chartRef.current,
+                    [
+                        ...klinedata.map((x)=>({
+                            close: parseFloat(x.close),
+                            high: parseFloat(x.high),
+                            low: parseFloat(x.low),
+                            open: parseFloat(x.open),
+                            timestamp: new Date(x.end)
+                        })).sort((x,y)=>(x.timestamp < y.timestamp ? -1 : 1)) || []
+                    ],
+                    {
+                        background: "#0e0f14",
+                        color: "white",
+                    }
+                )
+                chartManagerRef.current = chartManager    
+            }
+        }
+        init()
+    }, [market,chartRef])
+
+
+    return (
+        <>
+        <div
+        ref={chartRef}
+        style={
+            {
+                height:'520px',
+                width:'100%',
+                marginTop:4
+            }
+        }
+        >
+
+        </div>
+        </>
+    )
+}
+
+export default TradeView
